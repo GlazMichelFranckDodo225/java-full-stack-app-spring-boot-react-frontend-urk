85fb25b126166a047ad2e9e1cff96091a6326d88
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-02 15:42:25 +0200

    Creating Two React Functional Components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      My Todo Application
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
     </div>
   );
+}
+
+function FirstComponent() {
+  return(
+    <div className='FirstComponent'>First Component</div>
+  )
+}
+
+function SecondComponent() {
+  return(
+    <div className='SecondComponent'>Second Component</div>
+  )
 }
 
 export default App;
