59616befaa738dab7dda74a63ee3d102a9836c58
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-23 09:47:42 +0200

    Setting Up CounterButton as a Separate Module

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { PropTypes } from 'prop-types'
 import "./Counter.css";
+import CounterButton from './CounterButton';
 
 export default function Counter() {
     const [count, setCount] = useState(0); // Destructuring
@@ -33,46 +33,4 @@ export default function Counter() {
             />
         </>
     );
-}
-
-function CounterButton({ by, incrementMethod, deCrementMethod }) {
-    // const state = useState(0);
-    const [count, setCount] = useState(0); // Destructuring
-    // console.log(by);
-
-    function incrementCounterFunction() {
-        setCount(count + by);
-        incrementMethod(by);
-    }
-
-    function deCrementCounterFunction() {
-        setCount(count - by);
-        deCrementMethod(by);
-    }
-
-    return (
-        <div className="counter">
-            <div>
-                <button
-                    className="counterButton"
-                    onClick={incrementCounterFunction}
-                >+ {by}</button>
-
-                <button
-                    className="counterButton"
-                    onClick={deCrementCounterFunction}
-                >- {by}</button>
-            </div>
-        </div>
-    )
-}
-
-// To Type the "by" Property of the Counter Component
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-// To Set Default Value to the "by" Property of the Counter Component
-CounterButton.defaultProps = {
-    by: 1
 }
---
+++ src/components/counter/CounterButton.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { PropTypes } from 'prop-types';
+
+export default function CounterButton({ by, incrementMethod, deCrementMethod }) {
+    // const state = useState(0);
+    const [count, setCount] = useState(0); // Destructuring
+    // console.log(by);
+
+    function incrementCounterFunction() {
+        setCount(count + by);
+        incrementMethod(by);
+    }
+
+    function deCrementCounterFunction() {
+        setCount(count - by);
+        deCrementMethod(by);
+    }
+
+    return (
+        <div className="counter">
+            <div>
+                <button
+                    className="counterButton"
+                    onClick={incrementCounterFunction}
+                >+ {by}</button>
+
+                <button
+                    className="counterButton"
+                    onClick={deCrementCounterFunction}
+                >- {by}</button>
+            </div>
+        </div>
+    )
+}
+
+// To Type the "by" Property of the Counter Component
+CounterButton.propTypes = {
+    by: PropTypes.number
+}
+
+// To Set Default Value to the "by" Property of the Counter Component
+CounterButton.defaultProps = {
+    by: 1
+}
