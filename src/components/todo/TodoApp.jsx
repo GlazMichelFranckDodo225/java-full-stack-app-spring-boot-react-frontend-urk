97694db9e4b20d114f247baaf6c5ea37b7a9d924
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-15 05:51:54 +0100

    Removing Hard Coding from Welcome Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom';
 import './TodoApp.css';
 
 export default function TodoApp() {
@@ -9,7 +9,7 @@ export default function TodoApp() {
                 <Routes>
                     <Route path='/' element={<LoginComponent />}></Route>
                     <Route path='/login' element={<LoginComponent />}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
                     <Route path='*' element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
@@ -43,7 +43,7 @@ function LoginComponent() {
             console.log("Succes");
             setShowSuccessMessage(true);
             setShowErrorMessage(false);
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         } else {
             console.log("Failed");
             setShowSuccessMessage(false);
@@ -74,9 +74,13 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+    const {username} = useParams();
+
+    console.log(username);
+    
     return (
         <div className="WelcomeComponent">
-            <h1>Welcome !</h1>
+            <h1>Welcome {username} !</h1>
             <div>
                 Welcome Component
             </div>
