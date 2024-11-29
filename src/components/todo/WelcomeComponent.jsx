9c13764314914a532741b4bf46f3190a004cba51
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-29 14:49:56 +0100

    Adding Axios Library Dependency | Call Spring Boot Hello World REST API from React Hello World Component

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,15 +1,36 @@
-import { useParams, Link} from 'react-router-dom';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
 
 function WelcomeComponent() {
     const { username } = useParams();
 
-    // console.log(username);
+    function callHelloWorldRestApi() {
+        console.log("Called !");
+
+        axios.get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log("Cleanup !"));       
+    }
+    
+    function successfulResponse(response) {
+        console.log(response);      
+    }
+    
+    function errorResponse(error) {
+        console.log(error);        
+    }
 
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username} !</h1>
             <div>
                 Manage Your Todo - <Link to="/todos">Go Here.</Link>
+            </div>
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>
+                    Call Hello World REST API
+                </button>
             </div>
         </div>
     )
